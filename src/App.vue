85d64128666b0857e445be63e9e648abb3dc5e66
Dejeux-Antoine <150045949+Dejeux-Antoine@users.noreply.github.com>
2024-04-27 15:21:30 +0200

    fin tp (oublie commit)

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,40 @@
 <script setup lang="ts">
-import { onErrorCaptured } from 'vue'
+import { onErrorCaptured, ref } from 'vue'
 import { RouterLink, RouterView } from 'vue-router/auto'
 
 onErrorCaptured((err, instance, info) => {
   console.error('erreur : ', err, '\ninfo : ', info, '\ncomposant : ', instance)
   return true
 })
+
+const menuIsOpen = ref(false)
 </script>
 
 <template>
   <header>
-    <nav>
+    <button @pointerdown="menuIsOpen = !menuIsOpen"
+      aria-controls="mainNav"
+      aria-expanded="true"
+      class="rounded-full border-2 border-red-600 bg-red-300 px-2"
+    >
+      menu
+    </button>
+
+    <Transition
+    class="transition-transform duration-1000"
+    enter-from-class="-translate-x-full"
+    enter-to-class="translate-x-0"
+    leave-active-class="-translate-x-full"
+    >
+    <nav id="mainNav" v-show="menuIsOpen">
       <ul>
-        <li>
-          <RouterLink to="/" class="text-red-500 underline"> Accueil </RouterLink>
-        </li>
+        <li><RouterLink to="/">accueil</RouterLink></li>
+        <li><RouterLink to="/accordeon">accordeon</RouterLink></li>
+        <li><RouterLink to="/boucle">boucle</RouterLink></li>
+
       </ul>
-    </nav>
+  </nav>
+  </Transition>
   </header>
   <RouterView v-slot="{ Component }">
     <Suspense>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
-//
+import { ref } from 'vue'
+
 </script>
+
 
 <template>
   <h1 class="text-2xl">Bonjour monde !</h1>
